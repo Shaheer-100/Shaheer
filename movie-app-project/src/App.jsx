84edf9c558efcd6a260bcff2c2000a06494b53d5
Shaheer-100 <mhdshaheer923@gmail.com>
2025-10-01 08:13:22 +0000

    movie-react-app

---
+++ movie-app-project/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Movies from './pages/Movies';
+import Navbar from './components/Navbar';
+import MovieDetails from './pages/MovieDetails';
+
+function App() {
+  return (
+      <Router>
+            <div className="App">
+                    <Navbar />
+                            <Routes>
+                                      <Route path="/" element={<Home />} />
+                                                <Route path="/about" element={<About />} />
+                                                          <Route path="/contact" element={<Contact />} />
+                                                                    <Route path="/movies" element={<Movies />} />
+                                                                
+                                                                          <Route path="/movie/:id" element={<MovieDetails />} />
+                                                                            </Routes>
+                                                                                  </div>
+                                                                                      </Router>
+                                                                                        );
+                                                                                        }
+
+                                                                                        export default App;
---
+++ movie-app-project/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+      <div style={{ padding: '2rem', textAlign: 'center' }}>
+            <h1>About Us</h1>
+                  <p>This is the About page - we'll add the carousel here later!</p>
+                      </div>
+                        );
+                        };
+
+                        export default About;
